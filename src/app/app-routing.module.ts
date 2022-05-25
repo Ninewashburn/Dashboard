883d7ed403dc873e35ad979f6bb208cd3547cde7
Meynadier Renaud <cenarius63@hotmail.fr>
2022-05-25 22:39:36 +0200

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { DashComponent } from './dash/dash.component';
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { SalesComponent } from './sales/sales.component';
+
+
+const routes: Routes = [
+  { path: 'dashboard', component: DashComponent },
+  { path: 'ventes', component: SalesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
